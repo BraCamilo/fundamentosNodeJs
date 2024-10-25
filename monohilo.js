cc140fa2d937a85af6a288212faa3cb02231e643
BraCamilo <brayancalvo64@outlook.com>
2024-10-25 15:47:07 -0500

    MonoHilo

---
+++ monohilo.js
@@ -0,0 +1,8 @@
+console.log('Hola Mundo');
+
+let i = 0
+setInterval(function(){
+    console.log(i);
+    i++ 
+}, 1000);
+//La idea de trabjar con monohilo, es estar pendiente de que sigue despues de que, que llegue lo que tiene que llegar
