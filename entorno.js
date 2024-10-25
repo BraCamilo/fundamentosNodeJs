6120aae303b6e0f55e0c4af7fb85c84bb334f803
BraCamilo <brayancalvo64@outlook.com>
2024-10-25 16:40:13 -0500

    ejecucion con node js

---
+++ entorno.js
@@ -0,0 +1,6 @@
+let nombre = process.env.NOMBRE || 'Sin nombre';
+let web = process.env.WEB || '"Sin web"';
+
+console.log('hola ' + nombre);
+console.log('Esta es mi pagina '+ web);
+console.log('Hola mundo')
