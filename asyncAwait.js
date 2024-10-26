6ff59a6b00c7da13bc3ff4d1dbb508cdb9c9d0ae
BraCamilo <brayancalvo64@outlook.com>
2024-10-26 12:20:32 -0500

    Async/Await

---
+++ asyncAwait.js
@@ -0,0 +1,51 @@
+async function hola(nombre) {
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            console.log('Hola ' + nombre);
+            resolve(nombre);                       
+        }, 1000);
+    })
+        
+}
+
+        
+function hablar(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            console.log('Bla. bla, bla, bla');
+            resolve();
+            /* reject()    */     
+        }, 1000);
+    })
+}
+
+async function conversacion(nombre, veces, callback){
+    if(veces>0){
+        hablar(function(){
+            conversacion(nombre, --veces, callback)
+        })
+    }else{
+        adios(nombre, callback)
+    }
+}
+
+async function adios(nombre) {
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            console.log('Adios ' + nombre);
+            resolve();
+        }, 1000);
+
+    })    
+}
+
+async function main() {
+    let nombre = await hola('Brayan');
+    await hablar();  
+    await hablar();
+    await adios(nombre);
+    console.log('Proceso Terminado');
+    
+}
+
+main();
