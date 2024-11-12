25058625740c78131c94ffa470c42a5e580c67a6
BraCamilo <brayancalvo64@outlook.com>
2024-11-12 14:37:21 -0500

    http, os y casi process

---
+++ Modulos/http.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+/* Crearemos un servidor con tecnologia http, este servidor recibira un requerimiento y una respuesta las
+cuales se daran mediante la funcion de ruter */
+
+http.createServer(ruter).listen(3000);
+function ruter(req, res){
+    console.log('nueva peticion');
+    console.log(req.url);
+    
+    switch(req.url){
+        case '/hola':
+            res.write('Hola, que tal');
+            res.end();
+            break;
+        default:
+            res.write('Error 404, no se que quieres')
+            res.end();
+    }
+}
