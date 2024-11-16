b7e463ae3d59dacce1a4c8bdd6239e85d4ed9f54
BraCamilo <brayancalvo64@outlook.com>
2024-11-16 09:03:12 -0500

    Instalación de paquetes npm

--- Libro/Introduccion.js
+++ Libro/Introduccion.js
@@ -1,4 +1,4 @@
-function factorial(numero) {
+/* function factorial(numero) {
     if (numero == 0) {
         return 1;
     } else {
@@ -10,4 +10,12 @@ console.log(factorial(8));
 /* Algunas veces, para mostrar el resultado que produce un programa, la salida
 esperada se escribe después de el, con dos diagonales y una flecha en frente. "// → 40320" */
 
-console.log("Hola mundo \ncomo estan?");
+/* console.log("Hola mundo \ncomo estan?"); */
+
+/* console.log(typeof 4.5)
+// → number
+console.log(typeof "Hola"); */
+
+console.log(Math.min(1,5,7));
+
+
---
+++ Paquetes/npm/paquetes.js
@@ -0,0 +1,15 @@
+const isOdd = require('is-odd');
+
+console.log(isOdd(3));
+
+/* Primeramente haremos el uso del comando de iniciación de un paquete, con el comando 
+
+```jsx
+npm init
+```
+
+Esto nos permirira establecer he indicar a la maquina que haremos instalación de un paquete me diante el comand:
+
+```jsx
+npm install <nombrePaquete>
+``` */
