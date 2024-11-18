77964b0f31690592fd33a4932716d4d91ececeb1
BraCamilo <brayancalvo64@outlook.com>
2024-11-18 11:04:44 -0500

    uso de modulo bcrypt

--- Paquetes/npm/bcrypt.js
+++ Paquetes/npm/bcrypt.js
@@ -2,11 +2,19 @@ const bcrypt = require('bcrypt');
 
 const password = '1234segira!';
 
+
 bcrypt.hash(password, 5, function(err, hash){
     console.log(hash);
 
     bcrypt.compare(password, hash, function(err, res){
         console.log(res);
+
+        if(res){
+            console.log('La contraseña es similar');
+        }else{
+            console.log('La contrañesa no es similar');
+            
+        }
         
     })
     
